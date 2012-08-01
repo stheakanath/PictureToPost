4179a18b61eeb05972bdc3fe4b9dbf600b21640e
Sony Theakanath <stheakanath@gmail.com>
2012-08-01 11:19:37 -0700

    Initial Commit

---
+++ PictureToPost/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PictureToPost
+//
+//  Created by Thomas Theakanath on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PictureToPost/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PictureToPost
+//
+//  Created by Thomas Theakanath on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
